2194b591be36059b7361724c8887115747006599
Prince Conde <princeconde.pc@gmail.com>
2024-02-02 09:33:02 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <label>Name:</label>
+        <input />
+        <label>Contact:</label>
+        <input />
+        
+        <label>Address:</label>
+        <input />
+        <button >Submit</button>
+        <br />
+        <br />
+
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Contact</td>
+              <td>Address</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            </tr>
+          </tbody>
+        </table>
+
+      </header>
+
+    </div>
+  )
+  
+}
+
+export default App;
