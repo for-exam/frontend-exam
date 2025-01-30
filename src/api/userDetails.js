a4fc5382c29f7fb73c37395d0b24bff7650b36d9
jipenalver <jianchris2k14@gmail.com>
2025-01-30 10:10:04 +0800

    feat: added crud api fetches

---
+++ src/api/userDetails.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export async function getUserDetails() {
+  return await axios.get('http://127.0.0.1:8000/api/user_details')
+}
+
+export async function createUserDetails(formData) {
+  return await axios.post('http://127.0.0.1:8000/api/user_details', formData)
+}
+
+export async function updateUserDetails(formData, id) {
+  return await axios.put('http://127.0.0.1:8000/api/user_details/' + id, formData)
+}
+
+export async function deleteUserDetails(id) {
+  return await axios.delete('http://127.0.0.1:8000/api/user_details/' + id)
+}
